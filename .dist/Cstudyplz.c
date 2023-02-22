84f8614c28b62db11aa38fd85817379e1aed7d27
LTS2 <leewoojin982@gmail.com>
2023-02-22 16:26:02 +0900

    2번 수정한 것.

--- .dist/Cstudyplz.c
+++ .dist/Cstudyplz.c
@@ -5,11 +5,12 @@ int main(void)
     int time;
 
     printf("초를 시간으로 변환해드립니다 시 분 초 :");
-    scanf("%d", &time); // 타임에 값 받기
+    scanf_s("%d", &time, 40); // 타임에 값 받기
 
     int hour = time / 3600;
     int min = (time % 3600) / 60;
     int sec = (time % 3600) % 60;
+    // 2번 수정
 
     printf("입력하신 %d은 %d시 %d분 %d 초 입니다.", time, hour, min, sec);
     // 맥으로 올린다.
