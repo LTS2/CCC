83a123c886e3ee6a92d755c3412761f819268179
LTS2 <leewoojin982@gmail.com>
2023-02-22 14:29:56 +0900

    second 지금버ㅜㅌ

--- .dist/Cstudyplz.c
+++ .dist/Cstudyplz.c
@@ -3,12 +3,7 @@
 int main(void)
 {
 
-    printf("진ㅉ나 언제 되느 ㄴ거임 도대체");
-    printf("되긴하냐");
-
-    printf("진짜 도냐고");
-    pirntf("plzplzplzplz");
-    printf("ddd");
+    printf("지금부터 다시한다.");
 
     // 하하.ㅇㅇ
     // dfsf
