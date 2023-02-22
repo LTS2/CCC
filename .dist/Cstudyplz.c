e7d85f3b7c668f7b6ac1b760f1927d7bddb384cc
LTS2 <leewoojin982@gmail.com>
2023-02-22 14:28:39 +0900

    ㅇㅇㅇ
    ㅇㅇㄴㅁ

--- .dist/Cstudyplz.c
+++ .dist/Cstudyplz.c
@@ -6,5 +6,12 @@ int main(void)
     printf("진ㅉ나 언제 되느 ㄴ거임 도대체");
     printf("되긴하냐");
 
+    printf("진짜 도냐고");
+    pirntf("plzplzplzplz");
+    printf("ddd");
+
+    // 하하.ㅇㅇ
+    // dfsf
+
     return 0;
 }
